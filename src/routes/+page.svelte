12ee712463ca324788ae31819a7c285401449950
unknown <shreeramjadhav564@gmail.com>
2025-02-21 18:36:14 +0530

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    let title = "Welcome to My SvelteKit Website";
+  </script>
+  
+  <h1>{title}</h1>
+  <p>This is the main page of our demo SvelteKit website.</p>
+  <a href="/about">Go to About Page</a>scp -i "C:\Users\Malabar Tech\Downloads\my-sveltekit-app-key.pem" -r "C:\Users\Malabar Tech\my-sveltekit-app" ec2-user@15.206.89.160:~/
+
+  
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,3 @@
+<h1>About Us</h1>
+<p>This is the about page of our SvelteKit demo website.</p>
+<a href="/">Back to Home</a>
